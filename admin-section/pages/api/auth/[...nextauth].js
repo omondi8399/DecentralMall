f6f5771384b75d14ca803ec9518aaae46a53e243
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-18 17:17:42 +0300

    updated auth

--- admin-section/pages/api/auth/[...nextauth].js
+++ admin-section/pages/api/auth/[...nextauth].js
@@ -1,4 +1,6 @@
-    import NextAuth from 'next-auth'
+import clientPromise from '@/lib/mongodb'
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import NextAuth from 'next-auth'
     import GoogleProvider from 'next-auth/providers/google'
 
     export default NextAuth({
@@ -8,5 +10,6 @@
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET
         }),    
-    ]
+    ],
+    adapter: MongoDBAdapter(clientPromise)
     })
