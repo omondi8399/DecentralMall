d5f96c504bccc9cbc763792e7b60b7ceeefc3b7f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-18 23:33:32 +0300

    updated products page

--- admin-section/pages/products.js
+++ admin-section/pages/products.js
@@ -1,16 +1,33 @@
 import Layout from "@/components/Layout";
 import Link from "next/link"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Products() {
+    const [products, setProducts] = useState([])
     useEffect(() => {
-        axios.get('/products').then(response => {
-            console.log(response.data)
+        axios.get('/api/products').then(response => {
+            setProducts(response.data)
         })
     }, [])
     return (
         <Layout>
             <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new products</Link>
+            <table className="basic">
+                <thead>
+                    <tr>
+                        <td>Product name</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product, i) => (
+                        <tr key={i}>
+                            <td>{product.title}</td>
+                            <td>buttons</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Layout>
     )
 }
