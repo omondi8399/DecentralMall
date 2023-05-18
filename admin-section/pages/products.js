bafdc439564de9845d818a74ea87acb52e53d69c
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-18 23:09:29 +0300

    updated products page

--- admin-section/pages/products.js
+++ admin-section/pages/products.js
@@ -1,7 +1,13 @@
 import Layout from "@/components/Layout";
 import Link from "next/link"
+import { useEffect } from "react";
 
 export default function Products() {
+    useEffect(() => {
+        axios.get('/products').then(response => {
+            console.log(response.data)
+        })
+    }, [])
     return (
         <Layout>
             <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new products</Link>
