74e34c50653d9ded7aa24432a598713bde5a4cff
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-18 23:05:02 +0300

    Added Product api

---
+++ admin-section/pages/api/products.js
@@ -0,0 +1,18 @@
+import { mongooseConnect } from "@/lib/mongoose"
+import { Product } from "@/models/product"
+
+export default async function handle (req, res) {
+    const {method} = req
+    await mongooseConnect()
+
+    if(method === "GET") {
+        res.json(await Product.get())
+    }
+    if (method === "POST") {
+        const {title, description, price} = req.body
+        const productDoc = await Product.create({
+            title,description,price,
+        })
+        res.json(productDoc)
+    }
+}
