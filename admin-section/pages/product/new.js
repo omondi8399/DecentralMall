c8f86c88d21b870841c839af1bada37b864b6e71
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-19 10:09:27 +0300

    Updated pages

--- admin-section/pages/product/new.js
+++ admin-section/pages/product/new.js
@@ -1,35 +1,10 @@
 import Layout from "@/components/Layout";
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function NewProduct() {
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [price, setPrice] = useState('')
-    const [goToProducts, setGoToProducts] = useState(false)
-    const router = useRouter()
-    async function createProduct(e) {
-        e.preventDefault()
-        const data = {title,description,price}
-        await axios.post('/api/products', data)
-        setGoToProducts(true)
-    }
-    if(goToProducts) {
-        router.push('/products')
-    }
     return (
         <Layout>
-            <form onSubmit={createProduct}>
-            <h1>New Product</h1>
-            <label>Product name</label>
-            <input type="text" placeholder="product name" value={title} onChange={e => setTitle(e.target.value)} />
-            <label>Description</label>
-            <textarea placeholder="description" value={description} onChange={e => setDescription(e.target.value)}></textarea>
-            <label>Price (in USD)</label>
-            <input type="number" placeholder="price" value={price} onChange={e => setPrice(e.target.value)} />
-            <button type="submit" className="btn-primary">Save</button>
-            </form>
+            <ProductForm />
         </Layout>
     )
 }
