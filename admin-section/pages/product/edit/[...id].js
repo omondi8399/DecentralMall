d8dcc22cbffce717d126e7ded37f6abe33a20ad3
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-19 10:07:02 +0300

    Added Product edit

---
+++ admin-section/pages/product/edit/[...id].js
@@ -0,0 +1,24 @@
+import Layout from "@/components/Layout";
+import ProductForm from "@/components/ProductForm";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function EditProductPage() {
+    const [productInfo, setProductInfo] = useState(null)
+    const router = useRouter()
+    const {id}= router.query
+    useEffect(() => {
+        if (!id) {
+            return
+        }
+        axios.get('/api/products?id='+id).then(response => {
+            console.log(response.data)
+        })
+    }, [id])
+    return (
+        <Layout>
+            <ProductForm {...productInfo} />
+        </Layout>
+    )
+}
