e3207248e194c0f3e165a64a43317301805a1160
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-19 12:41:11 +0300

    Updated ProductForm

--- admin-section/components/ProductForm.js
+++ admin-section/components/ProductForm.js
@@ -1,10 +1,10 @@
 import { useState } from "react"
-import Layout from "./Layout"
 import axios from "axios"
 import { useRouter } from "next/router"
 
 
 export default function ProductForm ({
+    _id,
     title:existingTitle,
     description:existingDescription,
     price:existingPrice,
@@ -14,19 +14,23 @@ export default function ProductForm ({
     const [price, setPrice] = useState(existingPrice || '')
     const [goToProducts, setGoToProducts] = useState(false)
     const router = useRouter()
-    async function createProduct(e) {
+    async function saveProduct(e) {
         e.preventDefault()
         const data = {title,description,price}
-        await axios.post('/api/products', data)
+        if (_id) {
+            //update 
+            await axios.put('/api/products', {...data,_id})
+        } else {
+            //create 
+            await axios.post('/api/products', data)
+        }
         setGoToProducts(true)
     }
     if(goToProducts) {
         router.push('/products')
     }
     return (
-        <Layout>
-            <form onSubmit={createProduct}>
-            <h1>New Product</h1>
+            <form onSubmit={saveProduct}>
             <label>Product name</label>
             <input type="text" placeholder="product name" value={title} onChange={e => setTitle(e.target.value)} />
             <label>Description</label>
@@ -35,6 +39,5 @@ export default function ProductForm ({
             <input type="number" placeholder="price" value={price} onChange={e => setPrice(e.target.value)} />
             <button type="submit" className="btn-primary">Save</button>
             </form>
-        </Layout>
     )
 }
