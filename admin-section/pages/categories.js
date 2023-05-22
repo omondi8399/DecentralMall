b29f96e449e2a2f0e16ebb2fb50debb395881b81
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-22 16:41:48 +0300

    updated categories pages

--- admin-section/pages/categories.js
+++ admin-section/pages/categories.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Categories() {
+    const [editedCategory, setEditedCategory] = useState(null)
     const [name, setName] = useState('')
+    const [parentCategory, setParentCategory] = useState('')
     const [categories, setCategories] = useState([])
     useEffect(() => {
         fetchCategories()
@@ -16,26 +18,44 @@ export default function Categories() {
     }
     async function saveCategory(e){
         e.preventDefault()
-        await axios.post('/api/categories', {name})
+        const data = {name,parentCategory}
+        if (editedCategory) {
+            data._id = editedCategory._id
+            await axios.put('/api/categories', data)
+            setEditedCategory(null)
+        } else {
+            await axios.post('/api/categories', data)
+        }
         setName('')
         fetchCategories()
+    }
+    function editCategory(category){
+        setEditedCategory(category)
+        setName(category.name)
+        setParentCategory(category.parent?._id)
     }
     return (
         <Layout>
             <h1>Categories</h1>
-            <label>New Categories name</label>
+            <label>{editedCategory ? `Edit category ${editedCategory.name}` : 'Create new category'}</label>
             <form onSubmit={saveCategory} className="flex gap-1">
             <input className="mb-0" type="text" placeholder={'Category name'} 
             onChange={e => setName(e.target.value)}  value={name}/>
-            <select className="">
+            <select className="mb-0 " onChange={e => setParentCategory(e.target.value)}
+            value={parentCategory}>
                 <option value="0">No parent category</option>
+                {Categories.length > 0 && categories.map(category => (
+                    <option key={category} value={category._id}>{category.name}</option>
+                ))}
             </select>
-            <button type={'submit'} className="btn-primary py-1">Save</button>
+            <button type="submit" className="btn-primary py-1">Save</button>
             </form>
             <table className="basic mt-4">
                 <thead>
                     <tr>
                         <td>Category name</td>
+                        <td>Parent category</td>
+                        <td></td>
                     </tr>
                 </thead>
                 <tbody>
@@ -43,6 +63,11 @@ export default function Categories() {
                         category => (
                         <tr key={category}>
                             <td>{category.name}</td>
+                            <td>{category?.parent?.name}</td>
+                            <td>
+                                <button onClick={() => editCategory(category)} className="btn-primary mr-1">Edit</button>
+                                <button className="btn-primary">Delete</button>
+                            </td>
                         </tr>
                         ))}
                 </tbody>
