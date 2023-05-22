f96f1608f1ae8fbd281676c6313eab4185b6fe8e
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-22 19:05:37 +0300

    updated productsForm component

--- admin-section/components/ProductForm.js
+++ admin-section/components/ProductForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 import { useRouter } from "next/router"
 import Spinner from "./Spinner"
@@ -11,17 +11,25 @@ export default function ProductForm ({
     description:existingDescription,
     price:existingPrice,
     images:existingImages,
+    category:assignedCategory,
 }) {
     const [title, setTitle] = useState(existingTitle || '')
     const [description, setDescription] = useState(existingDescription || '')
+    const [category, setCategory] = useState(assignedCategory || '')
     const [price, setPrice] = useState(existingPrice || '')
     const [images, setImages] = useState(existingImages || [])
     const [goToProducts, setGoToProducts] = useState(false)
     const [isUploading, setIsUploading] = useState(false)
+    const [categories, setCategories] = useState([])
     const router = useRouter()
+    useEffect(() => {
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data)
+        })
+    }, [])
     async function saveProduct(e) {
         e.preventDefault()
-        const data = {title,description,price,images}
+        const data = {title,description,price,images, category}
         if (_id) {
             //update 
             await axios.put('/api/products', {...data,_id})
@@ -57,6 +65,13 @@ export default function ProductForm ({
             <form onSubmit={saveProduct}>
             <label>Product name</label>
             <input type="text" placeholder="product name" value={title} onChange={e => setTitle(e.target.value)} />
+            <label>Category</label>
+            <select value={category} onChange={e => setCategory(e.target.value)}>
+                <option value="">Uncategorised</option>
+                {categories.length > 0 && categories.map(c => (
+                    <option key={c} value={c.id}>{c.name}</option>
+                ))}
+            </select>
             <label>Photos</label>
             <div className="mb-2 flex flex-wrap gap-1">
                 <ReactSortable list={images} className="flex flex-wrap gap-1" setList={uploadImagesOrder}>
