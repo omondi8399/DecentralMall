77c211cf4a49ef947196df80195b406a558c3129
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-26 17:01:40 +0300

    NavBar component

--- admin-section/components/NavBar.js
+++ admin-section/components/NavBar.js
@@ -4,11 +4,17 @@ import {useRouter} from "next/router"
 
 export default function NavBar() {
     const inactiveLink = 'flex gap-1 p-1'
-    const activeLink = inactiveLink +'bg-white text-blue-900 rounded-l-lg'
+    const activeLink = inactiveLink +'bg-highlight text-black rounded-sm'
+    const inactiveIcon = 'w-6 h-6'
+    const activeIcon = inactiveIcon + 'text-primary'
     const router = useRouter()
     const {pathname} = router
+    async function logout() {
+        await router.push('/')
+        await signOut()
+    }
     return (
-        <aside className="text-white p-4">
+        <aside className="text-gray-500 p-4">
             <Link href={'/'} className="flex gap-1 mb-4 mr-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
@@ -19,7 +25,7 @@ export default function NavBar() {
         </Link>
         <nav className="flex flex-col gap-2">
             <link href={'/products'} className={pathname === '/' ? activeLink : inactiveLink}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class={pathname === '/' ? activeIcon: inactiveIcon}>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
                 Dashboard
@@ -50,7 +56,7 @@ export default function NavBar() {
                 </svg>
                 Settings
             </link>
-            <button onClick={() => signOut()} className="inactiveLink">
+            <button onClick={() => logout} className="inactiveLink">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
             </svg>
