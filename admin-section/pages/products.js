20848bea0ac491a4ef09d5ef52422f71c3226322
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-26 20:01:24 +0300

    updated products pages

--- admin-section/pages/products.js
+++ admin-section/pages/products.js
@@ -11,7 +11,7 @@ export default function Products() {
     }, [])
     return (
         <Layout>
-            <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new products</Link>
+            <Link className="btn-primary" href={'/products/new'}>Add new products</Link>
             <table className="basic mt-2">
                 <thead>
                     <tr>
