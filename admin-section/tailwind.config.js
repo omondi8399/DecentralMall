14fd2aa0ac6fd9db91df090dade3cb448824935a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-26 17:03:18 +0300

    updated tailwind.config

--- admin-section/tailwind.config.js
+++ admin-section/tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5542F6',
+        highLight: '#EAE8FB',
+        bgGray: '#fbfafd',
+      }
+    },
   },
   plugins: [],
 }
