16e6450ef30c0c7062481ef2ed2673d3cc000e48
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-26 19:58:58 +0300

    updated Layout component

--- admin-section/components/Layout.js
+++ admin-section/components/Layout.js
@@ -1,7 +1,10 @@
 import NavBar from "@/components/NavBar"
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useState } from "react"
+import Logo from "./Logo"
 
 export default function Layout({children}) {
+    const [showNav,setShowNav] = useState(false)
     const { data: session } = useSession()
     if (!session) {
         return (
@@ -15,8 +18,20 @@ export default function Layout({children}) {
 
     return (
         <div className="bg-bgGray min-h-screen">
-        <NavBar />
-        <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">{children}</div>
+            <div className="md:hidden flex items-center p-4">
+            <button onClick={() => setShowNav(true)}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+                </svg>
+            </button>
+            <div className="flex grow justify-center mr-6">
+                <Logo />
+            </div>
+            </div>
+            <div className="flex">
+            <NavBar show={showNav} />
+            <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">{children}</div>
+            </div>
         </div>
     )
 }
