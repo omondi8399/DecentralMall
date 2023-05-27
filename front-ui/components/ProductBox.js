3103f4190e8269258ab85c7493bb59cfe259f845
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-27 20:21:25 +0300

    Updated ProductBox component

--- front-ui/components/ProductBox.js
+++ front-ui/components/ProductBox.js
@@ -1,12 +1,13 @@
 import styled from "styled-components"
 import Button from "./Button"
 import CartIcon from "./icons/CartIcon"
+import Link from "next/link"
 
 const ProductWrapper = styled.div`
 
 `
 
-const WhiteBox =styled.div`
+const WhiteBox =styled(Link)`
     background-color: #fff;
     padding: 20px;
     height: 120px;
@@ -20,22 +21,46 @@ const WhiteBox =styled.div`
         max-height: 80px;
     }
 `
-const Title = styled.h2`
+const Title = styled(Link)`
     font-weight: normal;
     font-size: .9rem;
+    color:inherit;
+    text-decoration:none;
     margin:0;
+`
+const ProductInfoBox = styled.div`
+    margin-top: 5px;
+`
+const PriceRow = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 2px;
+`
+const Price = styled.div`
+    font-size: 1.5rem;
+    font-weight:bold;
 `
 
 export default function ProductBox({_id,title,description,price,images}) {
+    const url = '/product/'+_id
+
     return (
         <ProductWrapper>
-            <WhiteBox>
+            <WhiteBox href={url}>
                 <div>
                 <img src={images[0]} alt="" />
                 </div>
             </WhiteBox>
-            <Title>{title}</Title>
-            <Button primary><CartIcon /></Button>
+            <ProductInfoBox>
+            <Title href={url}>{title}</Title>
+            <PriceRow>
+                <div>
+                ${price}
+                </div>
+                <Button primary outline>Add to Cart</Button>
+            </PriceRow>
+            </ProductInfoBox>
         </ProductWrapper>
     )
 }
