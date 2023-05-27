52942919e763d4afbc9b309cb576c4d6c4f2d5f7
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-27 16:53:00 +0300

    Updated Button component

--- front-ui/components/Button.js
+++ front-ui/components/Button.js
@@ -1,12 +1,13 @@
 import styled, { css } from "styled-components"
 
-const StyledButton = styled.button`
-    border:0;
+export const ButtonStyle = css`
+border:0;
     padding: 5px 15px;
     border-radius:5px;
     cursor: pointer;
     display:inline-flex;
     align-items:center;
+    text-decoration: none;
     svg{
         height: 16px;
         margin-right:5px;
@@ -31,6 +32,9 @@ const StyledButton = styled.button`
             height: 20px;
         }
     `}
+`
+const StyledButton = styled.button`
+    ${ButtonStyle}
 `
 
 export default function Button({children,...rest}) {
