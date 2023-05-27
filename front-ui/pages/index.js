f47e254c322e9586cef2dc0eb17e0e23347d9f2d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-27 12:36:21 +0300

    updated index page

--- front-ui/pages/index.js
+++ front-ui/pages/index.js
@@ -1,5 +1,11 @@
+import Featured from "@/components/Featured";
+import Header from "@/components/Header";
+
 export default function Homepage() {
   return (
-    <div>test</div>
+    <div>
+      <Header />
+      <Featured />
+    </div>
   )
 }
