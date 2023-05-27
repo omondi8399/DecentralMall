02e8b27e889a2e2ba140fa3c0d568aa698f9e9a8
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-27 10:42:16 +0300

    updated ProductForm component

--- admin-section/components/ProductForm.js
+++ admin-section/components/ProductForm.js
@@ -94,8 +94,8 @@ export default function ProductForm ({
                 ))}
             </select>
             {propertiesToFill.length > 0 && propertiesToFill.map(p => (
-                <div className="flex gap-1" key={p}>
-                    <div>{p.name}</div>
+                <div className="" key={p}>
+                    <label>{p.name[0].toUpperCase()+p.name.substring(1)}</label>
                     <select value={productProperties[p.name]} 
                     onChange={(e) => setProductProp(p.name), e.target.value}>
                     {p.value.map(v => (
@@ -108,7 +108,7 @@ export default function ProductForm ({
             <div className="mb-2 flex flex-wrap gap-1">
                 <ReactSortable list={images} className="flex flex-wrap gap-1" setList={uploadImagesOrder}>
                 {!!images?.length && images.map(link => (
-                    <div key={link} className="h-24">
+                    <div key={link} className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200">
                         <img src={link} alt="" className="rounded-lg" />
                     </div>
                 ))}
@@ -118,13 +118,13 @@ export default function ProductForm ({
                         <Spinner />
                     </div>
                 )}
-                <label className=" w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1
-                text-gray-500 rounded-lg bg-gray-200">
+                <label className=" w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1
+                text-primary rounded-sm bg-white shadow-sm border border-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                 </svg>
                     <div>
-                    Upload
+                    Add image
                     </div>
                     <input type="file" onChange={uploadImages} className="hidden"/>
                 </label>
