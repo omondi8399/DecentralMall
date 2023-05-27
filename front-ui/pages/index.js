6b09e670eed43241121afe68f1ccc84c7d24c784
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-27 16:51:42 +0300

    Updated index page

--- front-ui/pages/index.js
+++ front-ui/pages/index.js
@@ -1,11 +1,22 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/product";
 
-export default function Homepage() {
+export default function Homepage({product}) {
   return (
     <div>
       <Header />
-      <Featured />
+      <Featured product={product}/>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = '320de2b12aa291ebdf213d29';
+  await mongooseConnect()
+  const product = await Product.findById(featuredProductId)
+  return {
+    props: {product: JSON.parse(JSON.stringify(product))},
+  }
 }
