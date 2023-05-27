db550a3f490aab13c9e80fd9078c44fbd7b60ec2
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-27 19:10:18 +0300

    Updated Index page

--- front-ui/pages/index.js
+++ front-ui/pages/index.js
@@ -1,13 +1,15 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import NewProducts from "@/components/NewProducts";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/product";
 
-export default function Homepage({product}) {
+export default function Homepage({featuredProduct,newProducts}) {
   return (
     <div>
       <Header />
-      <Featured product={product}/>
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} />
     </div>
   )
 }
@@ -15,8 +17,12 @@ export default function Homepage({product}) {
 export async function getServerSideProps() {
   const featuredProductId = '320de2b12aa291ebdf213d29';
   await mongooseConnect()
-  const product = await Product.findById(featuredProductId)
+  const featuredProduct = await Product.findById(featuredProductId)
+  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10})
   return {
-    props: {product: JSON.parse(JSON.stringify(product))},
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts))
+    },
   }
 }
