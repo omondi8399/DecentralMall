3e358f37a4cd123e51d299679b788b42d88c28df
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-27 19:11:07 +0300

    Updated app

--- front-ui/pages/_app.js
+++ front-ui/pages/_app.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components"
 const GlobalStyles = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 body{
+  background-color: #eee
   padding:0;
   margin:0;
   font-family: 'Roboto', sans-serif;
