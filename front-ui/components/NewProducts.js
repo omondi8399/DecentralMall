8053ec5cb9269c17fa7a4ac70b316b2cd9c7eb1d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-28 09:14:41 +0300

    Updated NewProducts component

--- front-ui/components/NewProducts.js
+++ front-ui/components/NewProducts.js
@@ -6,12 +6,17 @@ const ProductsGrid = styled.div`
     display:grid;
     grid-template-columns: 1f 1fr 1fr 1fr;
     gap: 20px;
-    padding-top: 30px;
+`
+const Title = styled.h2`
+    font-size: 2rem;
+    margin:30px 0 20px;
+    font-weight: normal;
 `
 
 export default function NewProducts({products}) {
     return(
         <Center>
+            <Title>New Arrivals</Title>
             <ProductsGrid>
             {products?.length > 0 && products.map(product => (
                 <ProductBox key={product} {...product} />
