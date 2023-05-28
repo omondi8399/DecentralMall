aa43c4e8a67a04918c02e9d1c6d1e2606df13f68
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-28 18:13:57 +0300

    updated checkout api

--- front-ui/pages/api/checkout.js
+++ front-ui/pages/api/checkout.js
@@ -1,10 +1,49 @@
-export default function handler(req,res) {
+import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/order";
+import { Product } from "@/models/product";
+const stripe = require('string')(process.env.STRIPE_SK)
+
+export default async function handler(req,res) {
     if (req.method !== 'POST') {
         res.json('should be a POST request')
         return
     }
-    const {name,email,city,postalCode,streetAddress,country,products} = req.body;
-    const productsIds = products.split(',')
+    const {name,email,city,postalCode,streetAddress,country,cartProducts} = req.body;
+    await mongooseConnect()
+    const productsIds = cartProducts
     const uniqueIds = [...new Set(productsIds)]
-    const productsInfos
+    const productsInfos = await Product.find({_id:uniqueIds})
+
+    let line_items = []
+    for (const productId of uniqueIds) {
+        const productInfo = productsInfos.find(p => p._id.toString() === productId)
+        const quantity = productsIds.filter(id => id === productId)?.length || 0 
+        if (quantity > 0 && productInfo) {
+            line_items.push({ 
+                quantity,
+                price_data: {
+                    currency: 'USD',
+                    product_data: {name:productInfo.title},
+                    unit_amount: quantity + productInfo.price * 100,
+                }
+            })
+        }
+    }
+    
+    const orderDoc = await Order.create({
+        line_items,name,email,city,postalCode,streetAddress,country,paid:false,
+    })
+
+   const session = await stripe.checkout.sessions.create({
+        line_items,
+        mode:'payment',
+        customer_email:email,
+        success_url: process.env.PUBLIC_URL + '/cart?success=1',
+        cancel_url: process.env.PUBLIC_URL + '/cart?canceled=1',
+        metadata: {orderId:orderDoc._id.toString()},
+    })
+
+    res.json({
+        url:session.url,
+    })
 }
