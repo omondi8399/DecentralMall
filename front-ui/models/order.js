05bbac7d039a4ccce099397d7a20d072d24aad2a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-28 18:11:38 +0300

    added order model

---
+++ front-ui/models/order.js
@@ -0,0 +1,14 @@
+const { Schema, models, model } = require("mongoose");
+
+const OrderSchema = new Schema({
+    line_items:Object,
+    name:String,
+    email:String,
+    city:String,
+    postalCode:String,
+    streetAddress:String,
+    country:String,
+    paid:Boolean,
+})
+
+export const Order = models?.Order || model('Order', OrderSchema)
