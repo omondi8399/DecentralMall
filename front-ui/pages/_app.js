3176becfc8d27d39a971c90533a6c7587a6d6079
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-28 09:12:50 +0300

    Updated app.js

--- front-ui/pages/_app.js
+++ front-ui/pages/_app.js
@@ -1,7 +1,8 @@
+import { CartContextProvider } from "@/components/CartContext";
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap');
 body{
   background-color: #eee
   padding:0;
@@ -14,7 +15,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
+      <CartContextProvider>
       <Component {...pageProps} />
+      </CartContextProvider>
     </>
   )
 }
