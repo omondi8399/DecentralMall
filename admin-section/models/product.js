3928575403043350f48da8c8fb96913b5285709a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-29 11:26:58 +0300

    Updated product model

--- admin-section/models/product.js
+++ admin-section/models/product.js
@@ -1,12 +1,14 @@
-const { Schema, model, models, default: mongoose } = require("mongoose");
+import mongoose, {model, Schema, models} from "mongoose";
 
 const ProductSchema = new Schema({
-    title:{type:String, required:true},
-    description: String,
-    price:{type: Number, required: true},
-    images:[{type: String}],
-    category: {type:mongoose.Types.ObjectId, ref:'Category'},
-    properties: {type: Object},
-})
+  title: {type:String, required:true},
+  description: String,
+  price: {type: Number, required: true},
+  images: [{type:String}],
+  category: {type:mongoose.Types.ObjectId, ref:'Category'},
+  properties: {type:Object},
+}, {
+  timestamps: true,
+});
 
-export const Product = models.Product || model('Product', ProductSchema)
+export const Product = models.Product || model('Product', ProductSchema);
