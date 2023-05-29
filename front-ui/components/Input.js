383fa23fb193e8ea5516531e5f4e5bc530f7d2a6
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-29 10:40:18 +0300

    Updated Input component

--- front-ui/components/Input.js
+++ front-ui/components/Input.js
@@ -1,12 +1,14 @@
-const styledInput = styled.input`
-    width: 100%;
-    padding: 5px;
-    margin-bottom: 5px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    box-sizing: border-box;
-`
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 5px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-sizing:border-box;
+`;
 
 export default function Input(props) {
-    return <styledInput {...props} />
-}  
+  return <StyledInput {...props} />
+}
