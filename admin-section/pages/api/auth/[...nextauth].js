a88c267af4bad0b112d77df895f595857fed87cc
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-29 11:23:14 +0300

    Updated [...nextauth] api

--- admin-section/pages/api/auth/[...nextauth].js
+++ admin-section/pages/api/auth/[...nextauth].js
@@ -1,37 +1,37 @@
-import clientPromise from '@/lib/mongodb'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-import NextAuth, { getServerSession } from 'next-auth'
+import NextAuth, {getServerSession} from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['rodgers.omosh@gmail.com']
+const adminEmails = ['dawid.paszko@gmail.com'];
 
 export const authOptions = {
-    providers: [
-        
-        GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET
-        }),    
-    ],
-    adapter: MongoDBAdapter(clientPromise),
-    callbacks: {
-        session: ({session,token,user}) => {
-            if (adminEmails.includes(session?.user?.email)) {
-
-            } else {
-                return session
-            }
-        },
+  secret: process.env.SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
     },
-    }
+  },
+};
 
-    export default NextAuth(authOptions)
+export default NextAuth(authOptions);
 
-    export async function isAdminRequest(req,res) {
-        const session = await getServerSession(req,res,authOptions)
-        res.status(401)
-        res.end()
-        if (!adminEmails.includes(session?.user?.email)) {
-            throw 'not an admin'
-        }
-    }
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
