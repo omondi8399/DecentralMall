e5444e12c95e807629fc08d32c2fb07f787f6931
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-29 10:38:34 +0300

    Updated Table component

--- front-ui/components/Table.js
+++ front-ui/components/Table.js
@@ -1,19 +1,19 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const styledTable = styled.table`
-    width: 100%
-    th{
-        text-align: left;
-        text-transform: uppercase;
-        color: #ccc;
-        font-weight: 600;
-        font-size: .7rem;
-    }
-    td{
-        border-top: 1px solid rgba(0,0,0,.1);
-    }
-`
+const StyledTable = styled.table`
+  width: 100%;
+  th{
+    text-align: left;
+    text-transform: uppercase;
+    color: #ccc;
+    font-weight: 600;
+    font-size: .7rem;
+  }
+  td{
+    border-top: 1px solid rgba(0,0,0,.1);
+  }
+`;
 
-export default function Table({props}) {
-    return <styledTable {...props} />
+export default function Table(props) {
+  return <StyledTable {...props} />
 }
