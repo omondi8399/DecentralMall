812f84446c75de82c0bf7534589676ea3125b846
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-29 10:39:48 +0300

    Updated NewProducts component

--- front-ui/components/NewProducts.js
+++ front-ui/components/NewProducts.js
@@ -1,27 +1,18 @@
-import styled from "styled-components"
-import Center from "./Center"
-import ProductBox from "./ProductBox"
+import styled from "styled-components";
+import Center from "@/components/Center";
+import ProductsGrid from "@/components/ProductsGrid";
 
-const ProductsGrid = styled.div`
-    display:grid;
-    grid-template-columns: 1f 1fr 1fr 1fr;
-    gap: 20px;
-`
 const Title = styled.h2`
-    font-size: 2rem;
-    margin:30px 0 20px;
-    font-weight: normal;
-`
+  font-size: 2rem;
+  margin:30px 0 20px;
+  font-weight: normal;
+`;
 
 export default function NewProducts({products}) {
-    return(
-        <Center>
-            <Title>New Arrivals</Title>
-            <ProductsGrid>
-            {products?.length > 0 && products.map(product => (
-                <ProductBox key={product} {...product} />
-            ))}
-        </ProductsGrid>
-        </Center>
-    )
+  return (
+    <Center>
+      <Title>New Arrivals</Title>
+      <ProductsGrid products={products} />
+    </Center>
+  );
 }
