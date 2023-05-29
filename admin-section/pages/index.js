cd33031f9efcb511e841864bd0d4b1cafb605920
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-29 10:47:19 +0300

    Updated index page

--- admin-section/pages/index.js
+++ admin-section/pages/index.js
@@ -1,21 +1,19 @@
-import Layout from "@/components/Layout"
-import {useSession} from "next-auth/react"
-import Image from "next/image"
+import Layout from "@/components/Layout";
+import {useSession} from "next-auth/react";
 
 export default function Home() {
-  const {data: session} = useSession()
-  if(!session) return
+  const {data: session} = useSession();
   return <Layout>
     <div className="text-blue-900 flex justify-between">
       <h2>
-      Hello, <b>{session?.user?.name}</b>
+        Hello, <b>{session?.user?.name}</b>
       </h2>
-      <div className="flex bg bg-gray-300 gap-1 text-block rounded-lg overflow-hidden">
-      <Image src={session?.user?.image} alt="" className="w-8 h-8" />
-      <span className="px-2">
-      {session?.user?.name}
-      </span>
-      </div>      
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
+      </div>
     </div>
   </Layout>
 }
